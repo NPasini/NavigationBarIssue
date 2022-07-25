8f2c09020910727bb70d2dbe93e36ac81a60439f
Nicolò Pasini <eberron@hotmail.it>
2022-07-25 22:53:45 +0200

    Added code to reproduce issue

--- NavBarIssue/ViewController.swift
+++ NavBarIssue/ViewController.swift
@@ -1,19 +1,46 @@
 //
-//  ViewController.swift
-//  NavBarIssue
+//  MyViewController.swift
+//  test
 //
-//  Created by Nicolò Pasini on 25/07/22.
+//  Created by Nicolò Pasini on 19/07/22.
 //
 
 import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
+class MyViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var containerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let childController = UIHostingController(
+            rootView: Text("Title")
+        )
+
+        // Uncomment following lines to check that in UIKit the issue is not present
+//        let childController = UIViewController()
+
+        addChild(childController)
+
+        childController.view.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(childController.view)
+        NSLayoutConstraint.activate([
+            childController.view.topAnchor.constraint(equalTo: containerView.topAnchor),
+            childController.view.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+            childController.view.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+            childController.view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+
+        childController.didMove(toParent: self)
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Uncomment following line to solve the issue with SwiftUI component
+//        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 }
-
